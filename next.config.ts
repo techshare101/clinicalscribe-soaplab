593f92ba0362965593b888c26950c9916293e9f4
Koll VossFam <valentin2v2000@gmail,com>
2025-12-04 00:09:43 -0600

    SoapLab full UI + scenarios + template system

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,11 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: "export",
+  basePath: "/clinicalscribe-soaplab",
+  images: {
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;
